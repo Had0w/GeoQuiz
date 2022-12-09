e60e00ed76a513d1692e1f0d0e946d955720306c
sklu <sergey.v.klyuev@gmail.com>
2022-12-10 00:12:45 +0300

    - added ViewModel for main activity

--- app/src/main/java/ru/klyuev/MainActivity.kt
+++ app/src/main/java/ru/klyuev/MainActivity.kt
@@ -2,7 +2,6 @@ package ru.klyuev
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
@@ -19,28 +18,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
 
-    private val TAG = "MainActivity"
-
-    private val questionsBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
-    private var currentIndex = 0
-    private var answeredQuestion = 0;
-    private var rightAnswers = 0;
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this)[QuizViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate called")
-        setContentView(R.layout.activity_main)
 
-        // added view model
-        ViewModelProvider(this)[QuizViewModel::class.java]
+        setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -71,59 +56,30 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
-    }
-
     private fun updateQuestion() {
-        val questionTextResId = questionsBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
 
-        trueButton.isEnabled = !questionsBank[currentIndex].answered
-        falseButton.isEnabled = !questionsBank[currentIndex].answered
+        trueButton.isEnabled = !quizViewModel.isAnswered()
+        falseButton.isEnabled = !quizViewModel.isAnswered()
     }
 
     private fun getPrevQuestion() {
-        currentIndex = if (currentIndex - 1 < 0) {
-            questionsBank.size -1
-        } else {
-            currentIndex - 1
-        }
+        quizViewModel.moveToPrev()
         updateQuestion()
     }
 
     private fun getNextQuestion() {
-        currentIndex = (currentIndex + 1) % questionsBank.size
+        quizViewModel.moveToNext();
         updateQuestion()
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
 
-        val currentRightQuestion = questionsBank[currentIndex].answer
+        val currentRightQuestion = quizViewModel.currentQuestionAnswer
 
         val messageResId = if (currentRightQuestion == userAnswer) {
-            ++rightAnswers
+            ++quizViewModel.rightAnswers
             R.string.correct_toast
         } else {
             R.string.incorrect_toast
@@ -131,14 +87,14 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
 
-        questionsBank[currentIndex].answered = true
+        quizViewModel.setAnswered()
 
         trueButton.isEnabled = false
         falseButton.isEnabled = false
 
-        ++answeredQuestion
+        ++quizViewModel.answeredQuestion
 
-        if (answeredQuestion == questionsBank.size) {
+        if (quizViewModel.isAllQuestionsAnswered()) {
             showResult()
         } else {
             getNextQuestion()
@@ -150,12 +106,9 @@ class MainActivity : AppCompatActivity() {
         dialog.setMessage(R.string.right_percent)
 
         dialog.setTitle(R.string.right_percent)
-        dialog.setMessage("${rightAnswers * 100 / questionsBank.size} %")
+        dialog.setMessage("${quizViewModel.getRightAnswersPercent()} %")
         dialog.setPositiveButton("Ok") {dialog, id ->
-            questionsBank.forEach { q -> q.answered = false }
-            currentIndex = 0
-            answeredQuestion = 0
-            rightAnswers = 0
+            quizViewModel.resetParameters()
             getNextQuestion()
             dialog.cancel()
         }
--- app/src/main/java/ru/klyuev/QuizViewModel.kt
+++ app/src/main/java/ru/klyuev/QuizViewModel.kt
@@ -1,18 +1,60 @@
 package ru.klyuev
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 
-private const val TAG = "QuizViewModel"
+class QuizViewModel : ViewModel() {
 
-class QuizViewModel: ViewModel() {
+    var currentIndex = 0
+    var answeredQuestion = 0;
+    var rightAnswers = 0;
 
-    init {
-        Log.d(TAG, "ViewModel instance created")
+    val currentQuestionAnswer: Boolean
+    get() = questionsBank[currentIndex].answer
+
+    val currentQuestionText: Int
+    get() = questionsBank[currentIndex].textResId
+
+    fun isAnswered(): Boolean {
+        return questionsBank[currentIndex].answered
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.d(TAG, "ViewModel instance about to be destroyed")
+    fun setAnswered() {
+        questionsBank[currentIndex].answered = true
     }
+
+    fun isAllQuestionsAnswered(): Boolean {
+        return answeredQuestion == questionsBank.size
+    }
+
+    fun getRightAnswersPercent(): Int {
+        return rightAnswers * 100 / questionsBank.size
+    }
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionsBank.size
+    }
+
+    fun moveToPrev() {
+        currentIndex = if (currentIndex - 1 < 0) {
+            questionsBank.size -1
+        } else {
+            currentIndex - 1
+        }
+    }
+
+    fun resetParameters() {
+        questionsBank.forEach { q -> q.answered = false }
+        currentIndex = 0
+        answeredQuestion = 0
+        rightAnswers = 0
+    }
+
+    private val questionsBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
 }
